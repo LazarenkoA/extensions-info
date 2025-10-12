83febea8b2acc99699b2af015838e9311ea22d8d
Артем <laz-mail@mail.ru>
2025-10-12 17:15:51 +0300

    cron

--- backend/go.mod
+++ backend/go.mod
@@ -56,6 +56,8 @@ require (
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
 	github.com/pelletier/go-toml/v2 v2.0.8 // indirect
+	github.com/robfig/cron v1.2.0 // indirect
+	github.com/robfig/cron/v3 v3.0.1 // indirect
 	github.com/rogpeppe/go-internal v1.13.1 // indirect
 	github.com/samber/lo v1.52.0 // indirect
 	github.com/stretchr/testify v1.10.0 // indirect
--- backend/internal/usecase/jobs/job.go
+++ backend/internal/usecase/jobs/job.go
@@ -2,13 +2,16 @@ package jobs
 
 import (
 	"context"
+	"fmt"
 	"github.com/LazarenkoA/extensions-info/internal/models"
 	"github.com/LazarenkoA/extensions-info/internal/usecase"
 	"github.com/gin-gonic/gin"
 	"github.com/pkg/errors"
+	"github.com/robfig/cron/v3"
 	"github.com/samber/lo"
 	"net/http"
 	"strconv"
+	"sync"
 )
 
 type repo interface {
@@ -26,9 +29,12 @@ type IWS interface {
 }
 
 type Job struct {
-	repo     repo
-	analyzer IAnalyzer
-	ws       IWS
+	repo       repo
+	analyzer   IAnalyzer
+	ws         IWS
+	cronJobs   *cron.Cron
+	cronJobsID map[int]cron.EntryID
+	mx         sync.Mutex
 }
 
 const (
@@ -40,9 +46,11 @@ const (
 
 func New(repo repo, analyzer IAnalyzer, ws IWS) *Job {
 	return &Job{
-		repo:     repo,
-		analyzer: analyzer,
-		ws:       ws,
+		repo:       repo,
+		analyzer:   analyzer,
+		ws:         ws,
+		cronJobs:   cron.New(),
+		cronJobsID: make(map[int]cron.EntryID),
 	}
 }
 
@@ -52,11 +60,7 @@ func (j *Job) Register(route *gin.RouterGroup) {
 	route.DELETE("/deleteCronSettings/:db_id", j.deleteCronSettings)
 	route.POST("/startManualAnalysis/:db_id", j.startManualAnalysis)
 
-	go j.runJobs()
-}
-
-func (j *Job) runJobs() {
-
+	j.cronJobs.Start()
 }
 
 func (j *Job) getCronSettings(ctx *gin.Context) {
@@ -87,12 +91,19 @@ func (j *Job) setCronSettings(ctx *gin.Context) {
 		return
 	}
 
+	parser := cron.NewParser(cron.Minute | cron.Hour | cron.Dom | cron.Month | cron.Dow | cron.Descriptor)
+	if _, err := parser.Parse(data.Schedule); err != nil {
+		usecase.ResponseError(ctx, fmt.Errorf("%q bad cron format", data.Schedule))
+		return
+	}
+
 	err = j.repo.SetSchedule(ctx, int32(id), data.Schedule)
 	if err != nil {
 		usecase.ResponseError(ctx, err)
 		return
 	}
 
+	j.Start(id, data.Schedule)
 	ctx.JSON(http.StatusOK, gin.H{})
 }
 
@@ -110,6 +121,7 @@ func (j *Job) deleteCronSettings(ctx *gin.Context) {
 		return
 	}
 
+	j.Stop(id) // todo запрос на отключение может попасть не на тот ПОД на котором работает джоба, поэтому нужно оповестить всех
 	ctx.JSON(http.StatusOK, gin.H{})
 }
 
@@ -121,13 +133,37 @@ func (j *Job) startManualAnalysis(ctx *gin.Context) {
 		return
 	}
 
-	logs := j.analyzer.RunAnalyzing(context.Background(), int32(id))
+	j.startAnalysis(id)
+	ctx.JSON(http.StatusOK, gin.H{})
+}
+
+func (j *Job) startAnalysis(dbID int) {
+	logs := j.analyzer.RunAnalyzing(context.Background(), int32(dbID))
 
 	go func() {
 		for log := range logs {
 			j.ws.Write(log)
 		}
 	}()
+}
 
-	ctx.JSON(http.StatusOK, gin.H{})
+func (j *Job) Stop(dbID int) {
+	j.mx.Lock()
+	defer j.mx.Unlock()
+
+	if jobID, ok := j.cronJobsID[dbID]; ok {
+		j.cronJobs.Remove(jobID)
+		delete(j.cronJobsID, dbID)
+	}
+}
+
+func (j *Job) Start(dbID int, schedule string) {
+	j.mx.Lock()
+	defer j.mx.Unlock()
+
+	id, _ := j.cronJobs.AddFunc(schedule, func() {
+		j.startAnalysis(dbID)
+	})
+
+	j.cronJobsID[dbID] = id
 }
